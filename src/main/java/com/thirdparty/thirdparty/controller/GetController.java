d8c5cc970898577e41f50b928bc05d456ce6a171
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-08-08 10:34:07 +0530

    GetController initialised

---
+++ src/main/java/com/thirdparty/thirdparty/controller/GetController.java
@@ -0,0 +1,18 @@
+package com.thirdparty.thirdparty.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thirdparty.thirdparty.postService.PostService;
+
+@RestController
+@RequestMapping("/api")
