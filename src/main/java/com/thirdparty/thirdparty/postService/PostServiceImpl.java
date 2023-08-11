988af3dd2f93c32e8970c0ce57f18bb08bfb67e8
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-08-11 15:07:26 +0530

    service implemented

---
+++ src/main/java/com/thirdparty/thirdparty/postService/PostServiceImpl.java
@@ -0,0 +1,16 @@
+package main.java.com.thirdparty.thirdparty.postService;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.val;
