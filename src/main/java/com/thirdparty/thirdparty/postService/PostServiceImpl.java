7e6bd46a6f3015477b7899c2c7e9162f76bbff50
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-08-15 10:12:34 +0530

    insert posts method added

--- src/main/java/com/thirdparty/thirdparty/postService/PostServiceImpl.java
+++ src/main/java/com/thirdparty/thirdparty/postService/PostServiceImpl.java
@@ -33,11 +33,19 @@ public class PostServiceImpl implements PostService{
         val response = restTemplate.exchange(url,HttpMethod.GET, httpEntity, List.class);
         return response.getBody();
     }
-    
+
     private HttpHeaders gethttpHeaders(){
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
         return headers;
+    }
+
+    @Override
+    public Map<String, Object> insertPosts(Map<String, Object> payload) {
+        HttpEntity <Map> httpEntity = new HttpEntity<>(payload,gethttpHeaders());
+        String url = sb.append(GET).toString();
+        val response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, Map.class);
+        return response.getBody();
     }
 }
