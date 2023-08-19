3b1cd769e4336e32879af5b0c78e59f3632a196d
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-08-19 09:16:44 +0530

    added rest template bean

--- src/main/java/com/thirdparty/thirdparty/ThirdPartyApplication.java
+++ src/main/java/com/thirdparty/thirdparty/ThirdPartyApplication.java
@@ -7,6 +7,11 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ThirdpartyApplication {
+
+    @Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
     public static void main(String[] args) {
 		SpringApplication.run(ThirdpartyApplication.class, args);
 	}
