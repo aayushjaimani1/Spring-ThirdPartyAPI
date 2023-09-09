26e3ae16af62e2243ac07cd87a1322c3cdda63ee
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-09 15:01:46 +0530

    controller initialised

---
+++ task/src/main/java/com/task/task/controller/userController.java
@@ -0,0 +1,19 @@
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.task.task.model.UserDetails;
+
+@RestController
+@RequestMapping("/submit-details")
+public class userController {
+    
+}
