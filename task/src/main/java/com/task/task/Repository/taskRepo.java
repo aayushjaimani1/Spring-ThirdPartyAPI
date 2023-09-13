8d94d10ad8fd84cff95f0046686ac4ddde832a3c
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-13 13:21:10 +0530

    JPA interface repo created

---
+++ task/src/main/java/com/task/task/Repository/taskRepo.java
@@ -0,0 +1,11 @@
+package com.task.task.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.task.task.model.UserDetails;
+
+@Repository
+public interface taskrepo extends JpaRepository<UserDetails,Integer> {
+    
+}
