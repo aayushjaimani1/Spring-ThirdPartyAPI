88b28841f17b19a5455ff402d3530b26c96b7205
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-14 07:23:59 +0530

    service interface initialised

---
+++ task/src/main/java/com/task/task/service/UserService.java
@@ -0,0 +1,11 @@
+package com.task.task.service;
+
+import java.util.List;
+
+import com.task.task.model.UserDetails;
+
+public interface UserService {
+    public UserDetails savesUserDetails(UserDetails user);
+
+    public List<UserDetails> getAllDetails();
+}
