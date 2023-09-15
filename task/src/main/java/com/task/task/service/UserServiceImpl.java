3c0c62fe3dfba6e2e02ac2ffa23c3f0f10eb38e5
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-15 12:30:24 +0530

    service interface implemented

---
+++ task/src/main/java/com/task/task/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.task.task.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.task.task.Repository.taskrepo;
+import com.task.task.model.UserDetails;
+
+@Service
+public class UserServiceImpl implements UserService{
+    
+}
