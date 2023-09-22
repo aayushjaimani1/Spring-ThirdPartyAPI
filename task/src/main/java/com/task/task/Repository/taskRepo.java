3a03a4eb28b4e63c1dc70fa82cb1e63bbf524aa9
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-22 17:03:33 +0530

    user repo completed

--- task/src/main/java/com/task/task/Repository/taskRepo.java
+++ task/src/main/java/com/task/task/Repository/taskRepo.java
@@ -1,4 +1,4 @@
-package com.task.task.Repository;
+package main.java.com.task.task.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
