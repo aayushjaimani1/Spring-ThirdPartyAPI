1a04a29a1adebbff7125ffd042bf6c76f1944a09
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-26 09:32:21 +0530

    application completed

--- task/src/main/java/com/task/task/TaskApplication.java
+++ task/src/main/java/com/task/task/TaskApplication.java
@@ -1,7 +1,7 @@
 package main.java.com.task.task;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskApplication {
