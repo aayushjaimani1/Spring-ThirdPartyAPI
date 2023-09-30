aed35e933373e34744fca25feed25ef7282dc38f
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-09-30 15:31:19 +0530

    rest added

--- task/src/main/java/com/task/task/controller/userController.java
+++ task/src/main/java/com/task/task/controller/userController.java
@@ -8,8 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.client.RestTemplate;
 
 import com.task.task.model.UserDetails;
 
